c2b3859dc75aa4747820f65198929763ae4c0b5e
mbsaloka <mbsaloka@gmail.com>
2024-12-08 16:10:31 +0700

    feat: change upload method using multer

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -3,6 +3,7 @@ const config = require("../config/config");
 const Image = require("../models/image");
 const fs = require('fs');
 const path = require('path');
+const multer = require('multer');
 
 const getAllImages= async (req, res) => {
   try {
@@ -22,47 +23,39 @@ const getImage = async (req, res) => {
 }
 
 const uploadImage = async (req, res) => {
-    const filePath = req.body.filePath;
-
-    if (!filePath) {
-      return res.status(400).json({ message: 'No file path provided' });
-    }
-
-    const fileName = path.basename(filePath);
-    const destPath = path.join(__dirname, '../../public/staticimages', fileName);
-
-    fs.copyFile(filePath, destPath, async (err) => {
-      if (err) {
-        return res.status(500).json({ message: 'Error copying file', error: err.message });
-      }
-
-      const image = new Image({
-        imageUrl: `http://localhost:3000/staticimages/${fileName}`,
-        totalEntity: req.body.totalEntity,
-      });
-
-      try {
-        const newImage = await image.save();
-        res.status(201).json({ message: 'File uploaded successfully', url: `/staticimages/${fileName}` });
-      } catch (err) {
-        res.status(400).json({ message: err.message });
-      }
-    });
+  if (!req.file) {
+    return res.status(400).json({ message: 'Tidak ada file yang diunggah' });
   }
 
-  const deleteImage = async (req, res) => {
-    try {
-      // Delete the image file from the static images folder
-      fs.unlinkSync(path.join(__dirname, '../../public', res.image.imageUrl));
+  const fileName = req.file.filename;
+  const fileUrl = `http://localhost:3000/staticimages/${fileName}`;
 
-      // Delete the image document from MongoDB
-      await res.image.deleteOne();
+  const image = new Image({
+    imageUrl: fileUrl,
+    totalEntity: req.body.totalEntity,
+  });
 
-      res.json({ message: 'Image deleted successfully', imageUrl: res.image.imageUrl });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  };
+  try {
+    const newImage = await image.save();
+    res.status(201).json({ message: 'File berhasil diunggah', url: fileUrl });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const deleteImage = async (req, res) => {
+  try {
+    // Delete the image file from the static images folder
+    fs.unlinkSync(path.join(__dirname, '../../public', res.image.imageUrl));
+
+    // Delete the image document from MongoDB
+    await res.image.deleteOne();
+
+    res.json({ message: 'Image deleted successfully', imageUrl: res.image.imageUrl });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 const favoriteImage = async (req, res) => {
     const { starred } = req.body;
--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 const { authenticate } = require('../middleware/auth');
 const {findImage} = require('../middleware/imageMiddleware');
 const {
@@ -10,9 +12,21 @@ const {
     favoriteImage,
 } = require('../controllers/imageController');
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, '../../public/staticimages'));
+    },
+    filename: (req, file, cb) => {
+      const fileName = Date.now() + path.extname(file.originalname);
+      cb(null, fileName);
+    }
+});
+
+const upload = multer({ storage });
+
 router.get('/', authenticate, getAllImages);
 router.get('/:id', authenticate,findImage, getImage);
-router.post('/upload', uploadImage);
+router.post('/upload', upload.single('image'), uploadImage);
 router.delete('/delete/:id', authenticate,findImage, deleteImage);
 router.patch('/favorite/:id', authenticate,findImage, favoriteImage);
 
